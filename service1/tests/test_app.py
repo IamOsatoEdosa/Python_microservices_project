a6b4eb7c78af77b55c43b080836de6f25fbe117e
IamOsatoEdosa <edosaosato@gmail.com>
2024-12-10 18:26:56 -0500

    updated test for service 1

--- service1/tests/test_app.py
+++ service1/tests/test_app.py
@@ -1,4 +1,9 @@
+import os
+import sys
 import pytest
+# Add the parent directory to PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
 
 @pytest.fixture
